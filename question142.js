c8f31d427ba27923e3c43a5e92904db29c1ed4ca
amnakifayat13 <hayatullahamna@gmail.com>
2024-04-18 10:30:58 +0500

    Day 48 Challenge

---
+++ question142.js
@@ -0,0 +1,7 @@
+/* Create a Promise that resolves with "Hello, World!" after 2 seconds.*/
+const helloPromise = new Promise((resolve) => {
+    setTimeout(() => {
+        resolve("Hello, World!");
+    }, 2000);
+});
+helloPromise.then((message) => console.log(message));
---
+++ question143.js
@@ -0,0 +1,13 @@
+//Show how to use the .then() and .catch() methods to handle Promise resolution and rejection.
+const conditionalPromise = new Promise((resolve, reject) => {
+    const success = Math.random() > 0.5;
+    if (success) {
+        resolve("Success!");
+    }
+    else {
+        reject(new Error("Failure"));
+    }
+});
+conditionalPromise
+    .then((result) => console.log(result))
+    .catch((error) => console.log(error.message));
